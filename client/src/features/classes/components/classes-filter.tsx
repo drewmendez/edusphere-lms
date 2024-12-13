a55b31e003f93379ddbd4b5c8be5d82c0492ffa9
Andrew Mendez <andrewjeffmendez30@gmail.com>
2024-12-14 01:52:29 +0800

    fix: prevent classes filter from resetting value if page is refreshed
    solution: filter values should depend on the url parameters

--- client/src/features/classes/components/classes-filter.tsx
+++ client/src/features/classes/components/classes-filter.tsx
@@ -6,8 +6,8 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ChevronDown } from "lucide-react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 interface ClassesFilterProps {
   path: string;
@@ -15,26 +15,36 @@ interface ClassesFilterProps {
 }
 
 export default function ClassesFilter({ path, status }: ClassesFilterProps) {
-  const { data: classes } = useGetClasses();
-  const navigate = useNavigate();
-
   const [isOpen, setIsOpen] = useState(false);
   const [filter, setFilter] = useState<{
     id: number | string;
     subject: string;
-  }>({ id: "all", subject: "All classes" });
+  }>();
+
+  const { data: classes } = useGetClasses();
+  const navigate = useNavigate();
+  const params = useParams();
+  const filterId = params?.filter!;
+
+  const subject =
+    classes?.find((item) => item.class_id === parseInt(filterId))
+      ?.class_subject || "All classes";
+
+  useEffect(() => {
+    setFilter({ id: filterId, subject: subject });
+  }, [subject]);
 
   return (
     <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
       <DropdownMenuTrigger asChild>
         <div className="text flex max-w-[350px] cursor-pointer items-center justify-between rounded-md border-2 p-2 text-lg">
-          <p>{filter.subject}</p>
+          <p>{filter?.subject}</p>
           <ChevronDown className={`${isOpen && "rotate-180"}`} />
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem
-          className={`flex w-[350px] cursor-pointer justify-between text-lg ${filter.subject === "All classes" && "bg-muted"}`}
+          className={`flex w-[350px] cursor-pointer justify-between text-lg ${filter?.subject === "All classes" && "bg-muted"}`}
           onClick={() => {
             setFilter({ id: "all", subject: "All classes" });
             navigate(`/${path}/${status}/all`);
@@ -45,7 +55,7 @@ export default function ClassesFilter({ path, status }: ClassesFilterProps) {
         {classes?.map((item) => (
           <DropdownMenuItem
             key={item.class_id}
-            className={`flex w-[350px] cursor-pointer justify-between text-lg ${filter.id === item.class_id && "bg-muted"}`}
+            className={`flex w-[350px] cursor-pointer justify-between text-lg ${filter?.id === item.class_id && "bg-muted"}`}
             onClick={() => {
               setFilter({ id: item.class_id, subject: item.class_subject });
               navigate(`/${path}/${status}/${item.class_id}`);
